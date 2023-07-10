f7256a6d93b62b2827a1df4637a8c3ff49a73c70
Saikh Mirsat <123721202+saikhmirsat786@users.noreply.github.com>
2023-07-10 14:09:15 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const { connection } = require('./config/db')
 const { UserRoutes } = require('./Routes/Users.routes')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.use('/users', UserRoutes)
 
